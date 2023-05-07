cbab87daae0dcaee1d9d3455d38413f9992ebd4f
Juan Diego <jdiegopm12@gmail.com>
2023-05-07 17:32:28 -0500

    Linter configured

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
   root: true,
+  env: {
+    es6: true,
+    node: true,
+    mocha: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'google',
+    // 'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
 };
