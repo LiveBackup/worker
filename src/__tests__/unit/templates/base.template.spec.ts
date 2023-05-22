da76d1d1bb21066542accb9552de2abb6c3507b8
Juan Diego <jdiegopm12@gmail.com>
2023-05-22 13:41:59 -0500

    Tests fixed

--- src/__tests__/unit/templates/base.template.spec.ts
+++ src/__tests__/unit/templates/base.template.spec.ts
@@ -11,23 +11,25 @@ describe('Unit Testing - Templates', () => {
   it('Build a html template', () => {
     const template = testTemplate.buildTemplate();
 
-    const templateSections = [
-      '<!DOCTYPE html>',
-      '<html lang="es">',
-      '<head>',
-      '<title>Test</title>',
-      '<meta charset="utf-8">',
-      '<style>',
-      '.html {}',
-      '</style>',
-      '</head>',
-      '<body>',
-      '<p>Hello world<p>',
-      '</body>',
-      '</html>',
-    ];
-
     expect(template.length).to.be.greaterThan(0);
-    expect(template).to.be.equal(templateSections.join(''));
+    expect(template).to.be.equal(`
+      <!DOCTYPE html>
+      <html lang="es">
+
+      <head>
+        <title>Test</title>
+        <meta charset="utf-8">
+
+        <style>
+          .html {}
+        </style>
+      </head>
+
+      <body>
+        <p>Hello world<p>
+      </body>
+
+      </html>
+    `);
   });
 });
